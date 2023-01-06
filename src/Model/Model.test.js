0bd83f8005a6f9f5eb96f8151935a4d921afb320
Alexander Burdiss <aburdiss@icloud.com>
2023-01-06 12:19:56 -0500

    Add React Native Track Player
    Update data model to sort by month first so it is easier to find time appropriate music first
    Update data model to use download links instead of session specific links

--- src/Model/Model.test.js
+++ src/Model/Model.test.js
@@ -1,13 +1,16 @@
 import { tracks } from './Model';
 
 describe('tracks dates are all formatted correctly', () => {
-  const years = Object.keys(tracks);
-  years.forEach((year) => {
-    tracks[year].forEach((track) => {
-      test(track.date, () => {
-        var timestamp = Date.parse(track.date);
+  const months = Object.keys(tracks);
+  months.forEach((month) => {
+    const years = Object.keys(tracks[month]);
+    years.forEach((year) => {
+      tracks[month][year].forEach((track) => {
+        test(track.date, () => {
+          var timestamp = Date.parse(track.date);
 
-        expect(isNaN(timestamp)).toBeFalsy();
+          expect(isNaN(timestamp)).toBeFalsy();
+        });
       });
     });
   });
