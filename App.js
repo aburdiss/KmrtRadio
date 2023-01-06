82d16ea7b2c24b7f452d847df7a99c1798334f5b
Alexander Burdiss <aburdiss@icloud.com>
2023-01-05 22:22:07 -0500

    Initial Commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+/**
+ * @function App
+ * @component
+ * @description The main page for the Kmart Radio Application. This app will
+ * stream Kmart in-store clips from the 70s through the 90s, that are available
+ * on Archive.org
+ * https://archive.org/details/attentionkmartshoppers
+ * Created 1/5/23 by Alexander Burdiss
+ * @returns {JSX.Element} JSX render Instructions
+ *
+ * @copyright 2023 Alexander Burdiss
+ * @author Alexander Burdiss
+ * @since 1/5/23
+ * @version 0.0.1
+ */
+export default function App() {
+  return (
+    <View>
+      <Text>Kmart Radio</Text>
+    </View>
+  );
+}
