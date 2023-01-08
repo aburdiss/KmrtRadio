e6c267f7c0d59e9845649eb222a62cb5a570bf0f
Alexander Burdiss <aburdiss@icloud.com>
2023-01-08 18:53:50 -0500

    Add links to all tapes from Archive.org

---
+++ src/Model/tracks.js
@@ -0,0 +1,402 @@
+export const tracks = {
+  January: {
+    1991: [
+      {
+        date: 'January, 1991',
+        img: 'https://archive.org/download/KmartJanuary1991/Jan%2091.jpg',
+        src: 'https://archive.org/download/KmartJanuary1991/Kmart%20January%201991.mp3',
+        link: 'https://archive.org/details/KmartJanuary1991',
+      },
+    ],
+    1992: [
+      {
+        date: 'January, 1992',
+        img: 'https://archive.org/download/KmartJanuary1992Generic/Jan.jpg',
+        src: 'https://archive.org/download/KmartJanuary1992Generic/Kmart%20January%201992%20%28Generic%29.mp3',
+        link: 'https://archive.org/details/KmartJanuary1992Generic',
+      },
+    ],
+  },
+  February: {
+    1990: [
+      {
+        date: 'February, 1990',
+        img: 'https://archive.org/download/KmartFebruary1990/feb%2090.jpg',
+        src: 'https://archive.org/download/KmartFebruary1990/Kmart%20February%201990.mp3',
+        link: 'https://archive.org/details/KmartFebruary1990',
+      },
+    ],
+    1992: [
+      {
+        date: 'February, 1992',
+        img: 'https://archive.org/download/KmartFebruary1992Generic/Feb.jpg',
+        src: 'https://archive.org/download/KmartFebruary1992Generic/Kmart%20February%201992%20%28Generic%29.mp3',
+        link: 'https://archive.org/details/KmartFebruary1992Generic',
+      },
+      {
+        date: 'February 2, 1992',
+        img: 'https://archive.org/download/KmartWeekOf02.02.1992/2.2.jpg',
+        src: 'https://archive.org/download/KmartWeekOf02.02.1992/Kmart%20Week%20of%2002.02.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf02.02.1992',
+      },
+      {
+        date: 'February 9, 1992',
+        img: 'https://archive.org/download/KmartWeekOf02.09.1992/2.9.jpg',
+        src: 'https://archive.org/download/KmartWeekOf02.09.1992/Kmart%20Week%20of%2002.09.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf02.09.1992',
+      },
+      {
+        date: 'February 16, 1992',
+        img: 'https://archive.org/download/KmartWeekOf02.16.1992/2.16.jpg',
+        src: 'https://archive.org/download/KmartWeekOf02.16.1992/Kmart%20Week%20of%2002.16.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf02.16.1992',
+      },
+      {
+        date: 'February 23, 1992',
+        img: 'https://archive.org/download/KmartWeekOf02.23.1992/2.23.jpg',
+        src: 'https://archive.org/download/KmartWeekOf02.23.1992/Kmart%20Week%20of%2002.23.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf02.23.1992',
+      },
+    ],
+  },
+  March: {
+    1990: [
+      {
+        date: 'March, 1990',
+        img: 'https://archive.org/download/KmartMarch1990/Mar%2090.jpg',
+        src: 'https://archive.org/download/KmartMarch1990/Kmart%20March%201990.mp3',
+        link: 'https://archive.org/details/KmartMarch1990',
+      },
+    ],
+    1991: [
+      {
+        date: 'March, 1991',
+        img: 'https://archive.org/download/KmartApril1991_201509/March%2091.jpg',
+        src: 'https://archive.org/download/KmartApril1991_201509/Kmart%20March%201991.mp3',
+        link: 'https://archive.org/details/KmartApril1991_201509',
+      },
+    ],
+    1992: [
+      {
+        date: 'March, 1992',
+        img: 'https://archive.org/download/KmartMarchGeneric/March.jpg',
+        src: 'https://archive.org/download/KmartMarchGeneric/Kmart%20March%20%28Generic%29.mp3',
+        link: 'https://archive.org/details/KmartMarchGeneric',
+      },
+      {
+        date: 'March 1, 1992',
+        img: 'https://archive.org/download/Kmart30thAnniversaryProgram/IMG_20160227_0002.jpg',
+        src: 'https://archive.org/download/Kmart30thAnniversaryProgram/Kmart%2030th%20Anniversary%20Program.mp3',
+        link: 'https://archive.org/details/Kmart30thAnniversaryProgram',
+      },
+      {
+        date: 'March 1, 1992',
+        img: 'https://archive.org/download/KmartWeekOf03.01.1992/3.1.jpg',
+        src: 'https://archive.org/download/KmartWeekOf03.01.1992/Kmart%20Week%20of%2003.01.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf03.01.1992',
+      },
+      {
+        date: 'March 8, 1992',
+        img: 'https://archive.org/download/KmartWeekOf03.08.1992/3.8.jpg',
+        src: 'https://archive.org/download/KmartWeekOf03.08.1992/Kmart%20Week%20of%2003.08.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf03.08.1992',
+      },
+      {
+        date: 'March 15, 1992',
+        img: 'https://archive.org/download/KmartWeekOf03.15.1992/3.15.jpg',
+        src: 'https://archive.org/download/KmartWeekOf03.15.1992/Kmart%20Week%20of%2003.15.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf03.15.1992',
+      },
+      {
+        date: 'March 22, 1992',
+        img: 'https://archive.org/download/KmartWeekOf03.22.1992/3.22.jpg',
+        src: 'https://archive.org/download/KmartWeekOf03.22.1992/Kmart%20Week%20of%2003.22.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf03.22.1992',
+      },
+      {
+        date: 'March 29, 1992',
+        img: 'https://archive.org/download/KmartWeekOf03.29.1992/3.29.jpg',
+        src: 'https://archive.org/download/KmartWeekOf03.29.1992/Kmart%20Week%20of%2003.29.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf03.29.1992',
+      },
+    ],
+  },
+  April: {
+    1991: [
+      {
+        date: 'April, 1991',
+        img: 'https://archive.org/download/KmartApril1991/April%2091.jpg',
+        src: 'https://archive.org/download/KmartApril1991/Kmart%20April%201991.mp3',
+        link: 'https://archive.org/details/KmartApril1991',
+      },
+    ],
+    1992: [
+      {
+        date: 'April, 1992',
+        img: 'https://archive.org/download/KmartApril1992Generic/April.jpg',
+        src: 'https://archive.org/download/KmartApril1992Generic/Kmart%20April%201992%20%28Generic%29.mp3',
+        link: 'https://archive.org/details/KmartApril1992Generic',
+      },
+      {
+        date: 'April 5, 1992',
+        img: 'https://archive.org/download/KmartWeekOf04.05.1992/4.5.jpg',
+        src: 'https://archive.org/download/KmartWeekOf04.05.1992/Kmart%20Week%20of%2004.05.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf04.05.1992',
+      },
+      {
+        date: 'April 12, 1992',
+        img: 'https://archive.org/download/KmartWeekOf04.12.1992/4.12.jpg',
+        src: 'https://archive.org/download/KmartWeekOf04.12.1992/Kmart%20Week%20of%2004.12.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf04.12.1992',
+      },
+      {
+        date: 'April 26, 1992',
+        img: 'https://archive.org/download/KmartWeekOf04.26.1992/4.26.jpg',
+        src: 'https://archive.org/download/KmartWeekOf04.26.1992/Kmart%20Week%20of%2004.26.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf04.26.1992',
+      },
+    ],
+  },
+  May: {
+    1988: [
+      {
+        date: 'May, 1988',
+        img: 'https://archive.org/download/May1988ReelToReel/IMG_20151013_0003.jpg',
+        src: 'https://archive.org/download/May1988ReelToReel/Kmart%20May%201988%20Reel%20to%20Reel.mp3',
+        link: 'https://archive.org/details/May1988ReelToReel',
+      },
+    ],
+    1991: [
+      {
+        date: 'May, 1991',
+        img: 'https://archive.org/download/KmartMay1991/May%2091.jpg',
+        src: 'https://archive.org/download/KmartMay1991/Kmart%20May%201991.mp3',
+        link: 'https://archive.org/details/KmartMay1991',
+      },
+    ],
+    1992: [
+      {
+        date: 'May, 1992',
+        img: 'https://archive.org/download/KmartMayGeneric/May.jpg',
+        src: 'https://archive.org/download/KmartMayGeneric/Kmart%20May%20%28Generic%29.mp3',
+        link: 'https://archive.org/details/KmartMayGeneric',
+      },
+      {
+        date: 'May 5, 1992',
+        img: 'https://archive.org/download/KmartWeekOf05.05.1992/5.5.jpg',
+        src: 'https://archive.org/download/KmartWeekOf05.05.1992/Kmart%20Week%20of%2005.05.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf05.05.1992',
+      },
+      {
+        date: 'May 10, 1992',
+        img: 'https://archive.org/download/KmartWeekOf05.10.1992/5-10.jpg',
+        src: 'https://archive.org/download/KmartWeekOf05.10.1992/Kmart%20Week%20of%2005.10.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf05.10.1992',
+      },
+      {
+        date: 'May 17, 1992',
+        img: 'https://archive.org/download/KmartWeekOf05.17.1992/5.17.jpg',
+        src: 'https://archive.org/download/KmartWeekOf05.17.1992/Kmart%20Week%20of%2005.17.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf05.17.1992',
+      },
+      {
+        date: 'May 24, 1992',
+        img: 'https://archive.org/download/KmartWeekOf05.24.1992_201903/5.24.jpg',
+        src: 'https://archive.org/download/KmartWeekOf05.24.1992_201903/Kmart%20Week%20of%2005.24.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf05.24.1992_201903',
+      },
+      {
+        date: 'May 31, 1992',
+        img: 'https://archive.org/download/KmartWeekOf05.31.1992/5.31.jpg',
+        src: 'https://archive.org/download/KmartWeekOf05.31.1992/Kmart%20Week%20of%2005.31.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf05.31.1992',
+      },
+    ],
+  },
+  June: {
+    1990: [
+      {
+        date: 'June, 1990',
+        img: 'https://archive.org/download/KmartJune1990/June%2090.jpg',
+        src: 'https://archive.org/download/KmartJune1990/Kmart%20June%201990.mp3',
+        link: 'https://archive.org/details/KmartJune1990',
+      },
+    ],
+    1992: [
+      {
+        date: 'June, 1992',
+        img: 'https://archive.org/download/KmartJune1992Generic/June.jpg',
+        src: 'https://archive.org/download/KmartJune1992Generic/Kmart%20June%201992%20%28Generic%29.mp3',
+        link: 'https://archive.org/details/KmartJune1992Generic',
+      },
+      {
+        date: 'June 7, 1992',
+        img: 'https://archive.org/download/KmartWeekOf06.07.1992/6.7.jpg',
+        src: 'https://archive.org/download/KmartWeekOf06.07.1992/Kmart%20Week%20of%2006.07.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf06.07.1992',
+      },
+      {
+        date: 'June 14, 1992',
+        img: 'https://archive.org/download/KmartWeekOf06.14.1992/6.14.jpg',
+        src: 'https://archive.org/download/KmartWeekOf06.14.1992/Kmart%20Week%20of%2006.14.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf06.14.1992',
+      },
+      {
+        date: 'June 21, 1992',
+        img: 'https://archive.org/download/KmartWeekOf06.21.1992/6.21.jpg',
+        src: 'https://archive.org/download/KmartWeekOf06.21.1992/Kmart%20Week%20of%2006.21.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf06.21.1992',
+      },
+      {
+        date: 'June 28, 1992',
+        img: 'https://archive.org/download/KmartWeekOf06.28.1992/6.28.jpg',
+        src: 'https://archive.org/download/KmartWeekOf06.28.1992/Kmart%20Week%20of%2006.28.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf06.28.1992',
+      },
+    ],
+  },
+  July: {
+    1990: [
+      {
+        date: 'July, 1990',
+        img: 'https://archive.org/download/KmartJuly1990/July%2090.jpg',
+        src: 'https://archive.org/download/KmartJuly1990/Kmart%20July%201990.mp3',
+        link: 'https://archive.org/details/KmartJuly1990',
+      },
+    ],
+    1991: [
+      {
+        date: 'July, 1991',
+        img: 'https://archive.org/download/KmartJuly1991/July%2091.jpg',
+        src: 'https://archive.org/download/KmartJuly1991/Kmart%20July%201991.mp3',
+        link: 'https://archive.org/details/KmartJuly1991',
+      },
+    ],
+    1992: [
+      {
+        date: 'July, 1992',
+        img: 'https://archive.org/download/KmartJuly1992Generic/July.jpg',
+        src: 'https://archive.org/download/KmartJuly1992Generic/Kmart%20July%201992%20%28Generic%29.mp3',
+        link: 'https://archive.org/details/KmartJuly1992Generic',
+      },
+      {
+        date: 'July 5, 1992',
+        img: 'https://archive.org/download/KmartWeekOf07.05.1992/7.5.jpg',
+        src: 'https://archive.org/download/KmartWeekOf07.05.1992/Kmart%20Week%20of%2007.05.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf07.05.1992',
+      },
+      {
+        date: 'July 12, 1992',
+        img: 'https://archive.org/download/KmartWeekOf07.12.1992/7.12.jpg',
+        src: 'https://archive.org/download/KmartWeekOf07.12.1992/Kmart%20Week%20of%2007.12.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf07.12.1992',
+      },
+      {
+        date: 'July 19, 1992',
+        img: 'https://archive.org/download/KmartWeekOf07.19.1992/7.19.jpg',
+        src: 'https://archive.org/download/KmartWeekOf07.19.1992/Kmart%20Week%20of%2007.19.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf07.19.1992',
+      },
+      {
+        date: 'July 26, 1992',
+        img: 'https://archive.org/download/KmartWeekOf07.26.1992/7.26.jpg',
+        src: 'https://archive.org/download/KmartWeekOf07.26.1992/Kmart%20Week%20of%2007.26.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf07.26.1992',
+      },
+    ],
+  },
+  August: {
+    1990: [
+      {
+        date: 'August, 1990',
+        img: 'https://archive.org/download/KmartAugust1990/Aug%2090.jpg',
+        src: 'https://archive.org/download/KmartAugust1990/Kmart%20August%201990.mp3',
+        link: 'https://archive.org/details/KmartAugust1990',
+      },
+    ],
+    1992: [
+      {
+        date: 'August, 1992',
+        img: 'https://archive.org/download/KmartAugust1992Generic/Aug.jpg',
+        src: 'https://archive.org/download/KmartAugust1992Generic/Kmart%20August%201992%20%28Generic%29.mp3',
+        link: 'https://archive.org/details/KmartAugust1992Generic',
+      },
+      {
+        date: 'August 9, 1992',
+        img: 'https://archive.org/download/KmartWeekOf08.09.1992/8.9.jpg',
+        src: 'https://archive.org/download/KmartWeekOf08.09.1992/Kmart%20Week%20of%2008.09.1992.mp3',
+        link: 'https://archive.org/details/KmartWeekOf08.09.1992',
+      },
+    ],
+  },
+  September: {
+    1990: [
+      {
+        date: 'September, 1990',
+        img: 'https://archive.org/download/KmartSeptember1990/Sep%2090.jpg',
+        src: 'https://archive.org/download/KmartSeptember1990/Kmart%20September%201990.mp3',
+        link: 'https://archive.org/details/KmartSeptember1990',
+      },
+    ],
+  },
+  October: {
+    1989: [
+      {
+        date: 'October, 1989',
+        img: 'https://archive.org/download/KmartOctober1989/IMG_20160227_0001.jpg',
+        src: 'https://archive.org/download/KmartOctober1989/Kmart%20October%201989.mp3',
+        link: 'https://archive.org/details/KmartOctober1989',
+      },
+    ],
+    1990: [
+      {
+        date: 'October, 1990',
+        img: 'https://archive.org/download/KmartOctober1990/Oct%2090.jpg',
+        src: 'https://archive.org/download/KmartOctober1990/Kmart%20October%201990.mp3',
+        link: 'https://archive.org/details/KmartOctober1990',
+      },
+    ],
+  },
+  November: {
+    1990: [
+      {
+        date: 'November, 1990',
+        img: 'https://archive.org/download/KmartNovember1990/Nov%2090.jpg',
+        src: 'https://archive.org/download/KmartNovember1990/Kmart%20November%201990.mp3',
+        link: 'https://archive.org/details/KmartNovember1990',
+      },
+    ],
+  },
+  December: {
+    1974: [
+      {
+        date: 'December, 1974',
+        img: 'https://archive.org/download/KmartDecember1974ReelToReel/Christmas%201974%20Spool.jpg',
+        src: 'https://archive.org/download/KmartDecember1974ReelToReel/Kmart%20December%201974%20Reel%20to%20Reel.mp3',
+        link: 'https://archive.org/details/KmartDecember1974ReelToReel',
+      },
+    ],
+    1990: [
+      {
+        date: 'December, 1990',
+        img: 'https://archive.org/download/KmartDecember1990/IMG_20160227_0004.jpg',
+        src: 'https://archive.org/download/KmartDecember1990/Kmart%20December%201990.mp3',
+        link: 'https://archive.org/details/KmartDecember1990',
+      },
+    ],
+    1991: [
+      {
+        date: 'December, 1992',
+        img: 'https://archive.org/download/KmartDecember1992/Dec.jpg',
+        src: 'https://archive.org/download/KmartDecember1992/Kmart%20December%201992.mp3',
+        link: 'https://archive.org/details/KmartDecember1992',
+      },
+      {
+        date: 'December, 1991',
+        img: 'https://archive.org/download/KmartDecember1992Revised/Dec%20Revise.jpg',
+        src: 'https://archive.org/download/KmartDecember1992Revised/Kmart%20December%201992%20Revised.mp3',
+        link: 'https://archive.org/details/KmartDecember1992Revised',
+      },
+    ],
+  },
+};
