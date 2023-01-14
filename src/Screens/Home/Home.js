f8674a5e7276fa33608b46b6e92a8fb7c4ae93b1
Alexander Burdiss <aburdiss@icloud.com>
2023-01-13 19:19:12 -0500

    Converts Project files to use TypeScript

--- src/Screens/Home/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import Player from '../../Components/Player/Player';
-
-export default function Home() {
-  return <Player />;
-}
---
+++ src/Screens/Home/Home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Player from '../../Components/Player/Player';
+
+/**
+ * @function Home
+ * @component
+ * @description The Home screen of the App.
+ * Created 1/6/23
+ * @returns {JSX.Element} JSX render instructions
+ *
+ * @copyright 2023 Alexander Burdiss
+ * @author Alexander Burdiss
+ * @since 1/13/23
+ * @version 1.1.0
+ */
+export default function Home() {
+  return <Player />;
+}
